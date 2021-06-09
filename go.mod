af7e86f3477437fe2546e9076cbda5ba7e464dba
dmaspataud <dorian.maspataud@gmail.com>
2021-06-09 13:55:18 +0200

    First commit.

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/dmaspataud/smolmon
+
+go 1.16
+
+require (
+	github.com/pterm/pterm v0.12.23 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,24 +1,58 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"sync"
+
+	"github.com/pterm/pterm"
+	"gopkg.in/yaml.v2"
 )
 
+type YamlTarget struct {
+	Name    string
+	Address string
+}
+
+type YamlConfig struct {
+	Targets []YamlTarget
+}
+
 func main() {
-	address := "https://www.google.com"
-	resp, err := http.Get(address)
+	c := YamlConfig{}
+	var wg sync.WaitGroup
+	err := yaml.Unmarshal(readConf(), &c)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println(resp.StatusCode)
+	for i := 0; i < len(c.Targets); i++ {
+		if c.Targets[i].Address != "" {
+			wg.Add(1)
+			checkHealth(c.Targets[i], &wg)
+		}
+	}
 }
 
-func readConf() {
-
+func readConf() []byte {
+	conf, err := ioutil.ReadFile("./targets.yaml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return conf
 }
 
-func checkHealth(address string) {
-
+func checkHealth(target YamlTarget, wg *sync.WaitGroup) {
+	defer wg.Done()
+	checkSpinner, _ := pterm.DefaultSpinner.Start("Checking ...", target.Name)
+	resp, err := http.Get(target.Address)
+	if err != nil {
+		checkSpinner.Fail(target.Name, " (", target.Address, ") => ", err)
+		return
+	}
+	if resp.StatusCode != 200 {
+		checkSpinner.Fail(target.Name, " (", target.Address, ") => ", resp.Status)
+	} else if resp.StatusCode == 200 {
+		checkSpinner.Success(target.Name, " (", target.Address, ")")
+	}
 }
